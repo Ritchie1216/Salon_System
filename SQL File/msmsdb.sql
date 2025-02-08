91484ea877d9fdc1e396db83f41ffe3e8663b5ed
Ritchie1216 <ritchie121600@gmail.com>
2025-02-08 23:15:24 +0800

    upload the sql file

--- SQL File/msmsdb.sql
+++ SQL File/msmsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2023 at 06:51 AM
+-- Generation Time: Feb 08, 2025 at 11:14 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -62,8 +62,22 @@ CREATE TABLE `tblappointment` (
   `ApplyDate` timestamp NULL DEFAULT current_timestamp(),
   `Remark` varchar(250) DEFAULT NULL,
   `Status` varchar(50) DEFAULT NULL,
-  `RemarkDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `RemarkDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `HairColor` varchar(7) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblappointment`
+--
+
+INSERT INTO `tblappointment` (`ID`, `AptNumber`, `Name`, `Email`, `PhoneNumber`, `AptDate`, `AptTime`, `Services`, `ApplyDate`, `Remark`, `Status`, `RemarkDate`, `HairColor`) VALUES
+(1, '319514619', 'lize', 'lize121600@gmail.com', 12345678, '2024-09-13', '05:48', 'Hair Wash', '2024-09-11 03:44:14', 'ok', '1', '2024-09-11 03:46:18', NULL),
+(2, '452424880', 'Richie Chah', 'ritchie121600@gmail.com', 12345678, '2024-09-12', '14:30', 'Body Spa', '2024-09-11 06:30:01', NULL, NULL, NULL, NULL),
+(3, '250367874', 'Richie Chah', 'ritchie121600@gmail.com', 12345678, '2024-09-12', '19:57', 'Deluxe Pedicure', '2024-09-11 07:54:45', 'po', '1', '2024-09-11 07:55:29', NULL),
+(4, '401945245', 'Richie Chah', 'ritchie121600@gmail.com', 7899545665, '2024-09-19', '10:25', 'Hair Cut', '2024-09-18 01:24:14', '2345678', '1', '2024-09-18 01:24:58', NULL),
+(5, '400699941', 'Richie Chah', 'ritchie121600@gmail.com', 12345678, '2024-09-20', '15:00', 'Loreal Hair Color(Full)', '2024-09-19 04:57:25', NULL, NULL, NULL, '#000000'),
+(6, '860618643', 'Richie Chah', 'ritchie121600@gmail.com', 124478321, '2025-01-25', '10:15', 'Normal Menicure', '2025-01-17 01:15:52', NULL, NULL, NULL, '#000000'),
+(7, '955540261', 'JAmes', 'james@gmail.com', 123456789, '2025-01-23', '11:32', 'Loreal Hair Color(Full)', '2025-01-21 03:27:18', NULL, NULL, NULL, '#000000');
 
 -- --------------------------------------------------------
 
@@ -90,7 +104,39 @@ INSERT INTO `tblcustomers` (`ID`, `Name`, `Email`, `MobileNumber`, `Gender`, `De
 (2, 'Rahul Singh', 'singh@gmail.com', 5565565656, 'Male', 'Taken haircut by him', '2023-12-08 11:10:02', '2023-12-11 04:15:02'),
 (5, 'Test user', 'testuser@gmail.com', 1234567890, 'Female', 'Test', '2023-12-08 11:10:02', '2023-12-11 04:15:10'),
 (6, 'Manish', 'manish@gmail.com', 9879879798, 'Male', 'vjhgjhghg;lk;lklnhfjkhkjfnkl\r\nlkjklfjlkjlkc jjlkj\r\nl;ljlkj lkcjtkrjkjne', '2023-12-08 11:10:02', '2023-12-11 04:15:10'),
-(7, 'Anuj kumar', 'ak@gmail.com', 1234567899, 'Transgender', 'Test', '2023-12-08 11:10:02', '2023-12-11 04:15:10');
+(7, 'Anuj kumar', 'ak@gmail.com', 1234567899, 'Transgender', 'Test', '2023-12-08 11:10:02', '2023-12-11 04:15:10'),
+(8, 'lize', 'lize121600@gmail.com', 128462836, 'Male', 'ok', '2024-09-11 03:47:46', NULL),
+(9, 'Richie Chah', 'ritchie121600@gmail.com', 123456789, 'Male', 'wertyu', '2024-09-18 00:55:26', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblhaircuts`
+--
+
+CREATE TABLE `tblhaircuts` (
+  `id` int(11) NOT NULL,
+  `haircutName` varchar(100) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `imagePath` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblhaircuts`
+--
+
+INSERT INTO `tblhaircuts` (`id`, `haircutName`, `description`, `imagePath`) VALUES
+(4, 'Hair1', '1', 'images/女发型1.jpg'),
+(5, 'Hair2', '2', 'images/女发型2.jpg'),
+(6, 'Hair3', '3', 'images/女发型3.jpg'),
+(7, 'Hair4', '4', 'images/女发型4.jpg'),
+(8, 'Hair5', '5', 'images/女发型6.jpg'),
+(9, 'Hair6', '6', 'images/男生发型1.jpg'),
+(10, 'Hair7', '8', 'images/男生发型2.webp'),
+(11, 'Hair8', '8', 'images/男生发型3.jpeg'),
+(12, 'Hair9', '9', 'images/男生发型4.jpeg'),
+(13, 'Hair10', '10', 'images/男生发型5.jpg'),
+(14, 'Hair11', '11', 'images/男生发型7.jpeg');
 
 -- --------------------------------------------------------
 
@@ -112,7 +158,30 @@ CREATE TABLE `tblinvoice` (
 
 INSERT INTO `tblinvoice` (`id`, `Userid`, `ServiceId`, `BillingId`, `PostingDate`) VALUES
 (1, 7, 1, 529212751, '2023-12-11 05:39:34'),
-(2, 7, 3, 529212751, '2023-12-11 05:39:34');
+(2, 7, 3, 529212751, '2023-12-11 05:39:34'),
+(3, 8, 6, 387411357, '2024-09-11 03:48:06'),
+(4, 8, 7, 387411357, '2024-09-11 03:48:06'),
+(5, 8, 1, 215951404, '2024-09-18 00:34:10'),
+(6, 8, 2, 215951404, '2024-09-18 00:34:10'),
+(7, 8, 3, 215951404, '2024-09-18 00:34:10'),
+(8, 8, 4, 215951404, '2024-09-18 00:34:10'),
+(9, 8, 5, 215951404, '2024-09-18 00:34:10'),
+(10, 8, 6, 215951404, '2024-09-18 00:34:10'),
+(11, 8, 7, 215951404, '2024-09-18 00:34:10'),
+(12, 8, 8, 215951404, '2024-09-18 00:34:10'),
+(13, 8, 9, 215951404, '2024-09-18 00:34:10'),
+(14, 8, 10, 215951404, '2024-09-18 00:34:10'),
+(15, 8, 11, 215951404, '2024-09-18 00:34:10'),
+(16, 8, 12, 215951404, '2024-09-18 00:34:10'),
+(17, 8, 15, 215951404, '2024-09-18 00:34:10'),
+(18, 8, 16, 215951404, '2024-09-18 00:34:10'),
+(19, 8, 17, 215951404, '2024-09-18 00:34:10'),
+(20, 8, 18, 215951404, '2024-09-18 00:34:10'),
+(21, 9, 16, 858997932, '2024-09-18 00:56:08'),
+(22, 8, 5, 388054094, '2024-09-18 01:25:31'),
+(23, 8, 11, 524539783, '2024-09-22 05:06:46'),
+(24, 8, 12, 524539783, '2024-09-22 05:06:46'),
+(25, 8, 15, 524539783, '2024-09-22 05:06:46');
 
 -- --------------------------------------------------------
 
@@ -137,7 +206,7 @@ CREATE TABLE `tblpage` (
 
 INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`, `Timing`) VALUES
 (1, 'aboutus', 'About Us', '        Our main focus is on quality and hygiene. Our Parlour is well equipped with advanced technology equipments and provides best quality services. Our staff is well trained and experienced, offering advanced services in Skin, Hair and Body Shaping that will provide you with a luxurious experience that leave you feeling relaxed and stress free. The specialities in the parlour are, apart from regular bleachings and Facials, many types of hairstyles, Bridal and cine make-up and different types of Facials & fashion hair colourings.', NULL, NULL, NULL, ''),
-(2, 'contactus', 'Contact Us', '        890,Sector 62, Gyan Sarovar, GAIL Noida(Delhi/NCR)', 'info@gmail.com', 7896541236, NULL, '10:30 am to 8:30 pm');
+(2, 'contactus', 'Contact Us', '191,Taman Sri Ampang 05050 Alor Serta Kedah', 'ritchie121600@gmail.com', 124478218, NULL, '10:30 am to 7:30 pm');
 
 -- --------------------------------------------------------
 
@@ -194,7 +263,8 @@ CREATE TABLE `tblsubscriber` (
 INSERT INTO `tblsubscriber` (`ID`, `Email`, `DateofSub`) VALUES
 (1, 'ani@gmail.com', '2023-12-09 07:32:33'),
 (2, 'rahul@gmail.com', '2023-12-09 07:32:33'),
-(3, 'ganesh@gmail.com', '2023-12-09 07:32:33');
+(3, 'ganesh@gmail.com', '2023-12-09 07:32:33'),
+(6, 'ritchie121600@gmail.com', '2025-01-07 06:58:44');
 
 --
 -- Indexes for dumped tables
@@ -217,6 +287,12 @@ ALTER TABLE `tblappointment`
 --
 ALTER TABLE `tblcustomers`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblhaircuts`
+--
+ALTER TABLE `tblhaircuts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tblinvoice`
@@ -257,19 +333,25 @@ ALTER TABLE `tbladmin`
 -- AUTO_INCREMENT for table `tblappointment`
 --
 ALTER TABLE `tblappointment`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tblcustomers`
 --
 ALTER TABLE `tblcustomers`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tblhaircuts`
+--
+ALTER TABLE `tblhaircuts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tblinvoice`
 --
 ALTER TABLE `tblinvoice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `tblpage`
@@ -287,7 +369,7 @@ ALTER TABLE `tblservices`
 -- AUTO_INCREMENT for table `tblsubscriber`
 --
 ALTER TABLE `tblsubscriber`
-  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
